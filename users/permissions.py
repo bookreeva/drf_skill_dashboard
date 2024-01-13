91744fd99e4a7438f8c96ebdd2ecf2d3f1724c1a
Oh Olya <chanfoxx@yandex.ru>
2024-01-13 23:25:25 +0300

    add user's permissions

---
+++ users/permissions.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from rest_framework.permissions import BasePermission
+
+
+class IsOwner(BasePermission):
+    """ Права доступа для владельца. """
+    message = 'Вы не являетесь владельцем профиля.'
+
+    def has_object_permission(self, request, view, obj):
+        """ Настраивает способ проверки разрешений. """
+        return request.user == obj
+
+
+class IsSuperUser(BasePermission):
+    """ Права доступа для супер пользователя. """
+
+    def has_permission(self, request, view):
+        """ Настраивает способ проверки разрешений. """
+        return request.user.is_superuser
